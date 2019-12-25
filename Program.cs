087d5ff8575f5c5d1b4cc66aee8777a84e21f010
Chuck Francis <bnsf5622@gmail.com>
2019-12-25 10:54:42 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using SharpDX.Direct3D9;
+
+namespace TheOrderOfThings
+{
+    class Program
+    {
+        static Main(string)[] Args)
+        {
+            
+            double number;
+            string opinion, size, age, shape, color, origin, material, purpose;
+            string noun;
+
+            double = 5.0;
+            opinion = "AWESOME";
+            size = "teensy-weensy";
+            age = "new";
+            shape = "oblong";
+            color = "Bright yellow";
+            origin = "AlphaCentaurian";
+            Material = "platinum";
+            purpose = "good";
+
+            noun = "dragons";
+
+
+            // Using the + with strings, doesn't add it concatenates! (sticks them together)
+            Console.WriteLine(number + opinion + size  + age + shape + color
+                    + origin + material + purpose + noun);
+        }
+    }
+}
