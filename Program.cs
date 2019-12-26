4dba8bb3ff9e75042f498a26a4c1a2f9bd4fc5f5
Chuck Francis <bnsf5622@gmail.com>
2019-12-26 13:39:31 -0500

    starting

--- Program.cs
+++ Program.cs
@@ -1,33 +1,30 @@
 ﻿using System;
-using SharpDX.Direct3D9;
 
 namespace TheOrderOfThings
 {
     class Program
     {
-        static Main(string)[] Args)
+        static void Main(string[] Args)
         {
-            
             double number;
             string opinion, size, age, shape, color, origin, material, purpose;
             string noun;
 
-            double = 5.0;
+            number = 5.0;
             opinion = "AWESOME";
             size = "teensy-weensy";
             age = "new";
             shape = "oblong";
-            color = "Bright yellow";
+            color = "BRIGHT yellow";
             origin = "AlphaCentaurian";
-            Material = "platinum";
+            material = "platinum";
             purpose = "good";
 
             noun = "dragons";
 
-
-            // Using the + with strings, doesn't add it concatenates! (sticks them together)
-            Console.WriteLine(number + opinion + size  + age + shape + color
-                    + origin + material + purpose + noun);
+                // Using the + with strings, doesn't add it concatenates! (sticks them together)
+                Console.WriteLine(number + opinion + size + age + shape + color
+                        + origin + material + purpose + noun);
         }
     }
 }
