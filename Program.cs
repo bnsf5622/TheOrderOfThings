a3f620d1804f44d863d2c2607dd8a7a2833a8e95
Chuck Francis <bnsf5622@gmail.com>
2019-12-26 15:30:35 -0500

    changes again

--- Program.cs
+++ Program.cs
@@ -4,23 +4,23 @@ namespace TheOrderOfThings
 {
     class Program
     {
-        static void Main(string[] Args)
+        public static void Main(string[] Args)
         {
             double number;
             string opinion, size, age, shape, color, origin, material, purpose;
             string noun;
 
             number = 5.0;
-            opinion = "AWESOME";
-            size = "teensy-weensy";
-            age = "new";
-            shape = "oblong";
-            color = "BRIGHT yellow";
-            origin = "AlphaCentaurian";
-            material = "platinum";
-            purpose = "good";
+            opinion = ".0 AWESOME ";
+            size = " teensy-weensy ";
+            age = " new ";
+            shape = "oblong ";
+            color = " BRIGHT yellow ";
+            origin = "AlphaCentaurian ";
+            material = "platinum ";
+            purpose = "good ";
 
-            noun = "dragons";
+            noun = "dragons ";
 
                 // Using the + with strings, doesn't add it concatenates! (sticks them together)
                 Console.WriteLine(number + opinion + size + age + shape + color
